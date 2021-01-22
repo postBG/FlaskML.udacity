5178f036ae3b3dca61e6b3fef71cb15d2d1e4d70
postBG <profile2697@gmail.com>
2021-01-22 15:18:47 +0900

    test again

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home</h3><h2>I love Home Alone</h2>"
+    html = "<h3>Sklearn Prediction Home</h3><h2>I love my Home</h2>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
